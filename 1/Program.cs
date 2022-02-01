c1199b28640919dc9e7a1fae550b591346def8f3
Никита Бурмистров <16nik14bur12evgen@gmail.com>
2022-02-01 11:04:00 +0500

    Добавьте файлы проекта.

---
+++ 1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Введите количество элементов N:");
+                int N = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Масив:");
+                int[] a = new int[N];
+                Random r = new Random();
+                for (int i = 0; i < a.Length; i++)
+                {
+                    a[i] = r.Next(-10, 11);
+                    Console.Write(a[i] + "\t");
+                }
+                Console.WriteLine("\t");
+                int x = 0;
+                for (int i = 0; i < a.Length; i++)
+                {
+                    if (a[i] > 0)
+                    {
+                        x++;
+                    }
+                }
+                Console.WriteLine("Количество положительных чисел:" + x);
+                int y = 0;
+                for (int i = 0; i < a.Length; i++)
+                {
+                    if (a[i] < 0)
+                    {
+                        y++;
+                    }
+                }
+                Console.WriteLine("Количество отрицательных чисел:" + y);
+                int z = 0;
+                for (int i = 0; i < a.Length; i++)
+                {
+                    if (a[i] == 0)
+                    {
+                        z++;
+                    }
+                }
+                Console.WriteLine("Количество нулей:" + z);
+        }
+    }
+}
